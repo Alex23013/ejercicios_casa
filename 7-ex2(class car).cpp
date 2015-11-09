0465060c714627d6f0a2dad9d32187db5c6e6f40
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-09 15:41:27 -0500

    Delete 7-ex2(class car).cpp

--- 7-ex2(class car).cpp
+++
@@ -1,7 +0,0 @@
-class Car : public Vehicle { // Makes Car inherit from Vehicle
-  string style ;
-public :
-  Car(const string & myLicense , const int myYear , const string & myStyle )
-   :Vehicle ( myLicense , myYear ), style ( myStyle ) {}
-  const string & getStyle () { return style ;}
- };
