e990bd887907a1e0d69da79d2df3c4fb91c360a6
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-09 16:02:26 -0500

    Create 8-ex14(Integer array class).cpp

---
+++ 8-ex14(Integer array class).cpp
@@ -0,0 +1,13 @@
+class IntegerArray { 
+public: 
+  int *data;
+  int size;
+};
+ 
+int main() { 
+  IntegerArray arr;
+  arr.size = 2;
+  arr.data = new int[arr.size];
+  arr.data[0] = 4; arr.data[1] = 5;
+  delete[] a.data;
+}
