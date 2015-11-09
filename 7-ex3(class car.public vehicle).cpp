3a573e2e9d9ef491651c2f23a906d1e8af127e44
Alex23013 <alecornejolupa2013@gmail.com>
2015-11-09 15:41:53 -0500

    Delete 7-ex3(class car.public vehicle).cpp

--- 7-ex3(class car.public vehicle).cpp
+++
@@ -1,9 +0,0 @@
-class Car : public Vehicle { // Makes Car inherit from Vehicle 
-  string style ; 
-public :
-  Car(const string & myLicense , const int myYear , const string & myStyle )
-   : Vehicle ( myLicense , myYear ), style ( myStyle ) {}
-  const string getDesc () // Overriding this member function
-    { return stringify ( year ) + ’ ’ + style + ": " + license ;}
-  const string & getStyle () { return style ;} 
-}; 
